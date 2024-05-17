15000c009c9cb0e03c07a662c841238415606c5a
isaquewillas <eebmp.iwvl@educacao.arcos.mg.gov.br>
2024-05-16 21:51:29 -0300

    iniciarOlaMundo.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+
+    public static void main(String[] args) {
+    int num1,num2,soma;
+Scanner scanner = new Scanner (System.in);
+
+
+System.out.println("digite o primeiro numero");
+num1=scanner.nextInt();
+System.out.println("digite o segundo numero");
+num2 = scanner.nextInt();
+soma = num1+num2;
+
+System.out.println("result");
+System.out.println("A soma é: +soma");
+    }
+  
+ }
+
+
+
